c939a42c665870d8cd57ee8f64117eecaaeae00b
tmw-mm <toby@mirrorandmountain.com>
2025-08-24 22:16:35 +0000

    Update author info

--- src/Feature.php
+++ src/Feature.php
@@ -8,15 +8,21 @@ class Feature extends Extension
 {
     final protected function configure(): void
     {
-        $this->author             = 'MIRROR AND MOUNTAIN';
+       $this->author = [
+            'name'    => 'MIRROR AND MOUNTAIN',
+            'link'    => 'https://mirrorandmountain.com',
+            'support' => 'https://mirrorandmountain.com/support'
+        ];
+
         $this->hasIncludes        = true;
         $this->hasAssets          = true;
         $this->putScriptsInFooter = true;
         $this->hasComponents      = true;
+        $this->category           = 'styles';
 
         add_filter($this->name . '_user_switch_label', 
             function ( $value ) {
-                return 'Enable Single Page Loading.'; 
+                return 'Enable or disable Single Page Loading.'; 
             }
         );
     }
